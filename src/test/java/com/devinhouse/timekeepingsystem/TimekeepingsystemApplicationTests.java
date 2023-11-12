d563308c86cb451811c27819bdf42fedcf636d7d
lfogawa <lfogawa0@gmail.com>
2023-11-12 11:03:22 -0300

    initial commit

---
+++ src/test/java/com/devinhouse/timekeepingsystem/TimekeepingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinhouse.timekeepingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimekeepingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
